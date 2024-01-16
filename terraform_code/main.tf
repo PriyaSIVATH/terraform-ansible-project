d0ab3e78de7c7ee69305e3c8adc52db7c2456358
Priyanga <priyanga6418@gmail.com>
2024-01-16 18:25:52 +0000

    main.tf mistake corrected

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = us-east-1 
+    region = "us-east-1" 
 }
 
 resource "aws_instance" "target-server" {
-    ami = ami-0c7217cdde317cfec
-    instance_type = t2.micro
+    ami = "ami-0c7217cdde317cfec"
+    instance_type = "t2.micro"
 }
