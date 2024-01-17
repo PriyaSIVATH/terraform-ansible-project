bec385db6f1b4d04f395b4a6661b9a1b83bf837b
Priyanga <priyanga6418@gmail.com>
2024-01-17 11:12:09 +0000

    modified main with authentication details

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -1,8 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "us-east-1" 
+    access_key = var.access_key_value
+    secret_key = var.secret_key_value
+}
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+    key_name = var.key_name
+    public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+    content = tls_private_key.rsa_4096.private_key_pem
+    filename = var.key_name
 }
 
 resource "aws_instance" "target-server" {
-    ami = "ami-0c7217cdde317cfec"
-    instance_type = "t2.micro"
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    key_name = aws_key_pair.key_pair.key_name
+
+    tags = {
+      Name = var.tag_name
+    }
 }
+
+resource "aws_security_group" "terraform_sg" {
+  name        = "Security group using Terraform"
+  description = "Security group using Terraform"
+  vpc_id      = var.vpc_id_value
+
+
+ ingress {
+    description	     = "HTTP"
+    from_port        = var.http_port
+    to_port          = var.http_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
+
+ingress {
+    description	     = "SSH"
+    from_port        = var.ssh_port
+    to_port          = var.ssh_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ tags = {
+    Name = "tf_securitygroup"
+  }
+}
+
